a58aa1efe9249dd3628ee866769618f3f9120e34
Rodolfo Berlezi <dodozaum95@gmail.com>
2024-10-28 10:24:16 +0000

    Improve showing book list

--- src/content/TheSaga.js
+++ src/content/TheSaga.js
@@ -3,8 +3,7 @@ import { Button, Center, Image, Link, Stack } from '@chakra-ui/react'
 import TranslatedLabel from '../components/TranslatedLabel'
 import TranslatedHeading from '../components/TranslatedHeading'
 import { powerHeart } from '../utils/links.json'
-import bookWithEffects from '../images/bookCovers/bookWithEffects.png'
-import bookComingSoon from '../images/bookCovers/book_coming_soon-no-bg.png'
+import ph1CoverDetailed from '../images/bookCovers/bookCoverDetailed.jpg'
 import phLogoCor from '../images/power-heart/logo-ph-cor.png'
 import { amazonButtonColor } from '../utils/constants'
 
@@ -34,17 +33,19 @@ const TheSaga = () => {
       title: 'thesaga.ph1',
       launched: true,
       link: powerHeart.ph1Kindle,
-      image: bookWithEffects,
+      image: ph1CoverDetailed,
     },
     {
       title: 'thesaga.ph2',
       launched: false,
-      image: bookComingSoon,
+      image: 'bookComingSoon',
+      expected: '2025',
     },
     {
       title: 'thesaga.ph3',
       launched: false,
-      image: bookComingSoon,
+      image: 'bookComingSoon',
+      expected: '2026',
     },
   ]
 
@@ -56,7 +57,10 @@ const TheSaga = () => {
       <Stack id="books" width={{ lg: '80vw' }} minHeight={510} gap={10} mt={20} py={2} mx="auto">
         <Stack mb={10}>
           <Center>
-            <TranslatedHeading props={{ fontSize: 56 }} text={'thesaga.title'} />
+            <TranslatedHeading props={{ fontSize: 42 }} text={'thesaga.title'} />
+          </Center>
+          <Center>
+            <TranslatedHeading props={{ fontSize: 56 }} text={'thesaga.name'} />
           </Center>
           <Center>
             <TranslatedLabel props={{ fontSize: 26 }} text={'thesage.available'} />
@@ -66,8 +70,14 @@ const TheSaga = () => {
           {books.map((book) => {
             return (
               <Stack key={book.title} gap={5} mb={{ sm: 20, lg: 0 }}>
-                <Center>
-                  <Image width={40} src={book.image} />
+                <Center border={'solid'} borderColor={'black'}>
+                  {book.launched ? (
+                    <Image width={320} height={480} src={book.image} />
+                  ) : (
+                    <Center width={320} height={480} background={'black'} color={'white'} textAlign={'center'} fontSize={24} fontWeight={600}>
+                      <TranslatedLabel text={'thesaga.writing'} />
+                    </Center>
+                  )}
                 </Center>
                 <Center>
                   <strong>
